e727ada109042f402efc2d15e34eb6bbad488d16
Vinicius Morgado <34577818+viniciusmorgado@users.noreply.github.com.>
2024-10-30 19:25:11 -0300

    Implements Products controller

--- src/SkiStore.Api/Controllers/ProductsController.cs
+++ src/SkiStore.Api/Controllers/ProductsController.cs
@@ -38,4 +38,31 @@ public class ProductsController : ControllerBase
         // TODO: Don't return anything beyond the 200 HTTTP response if frontend don't need.
         return product;
     }
+    
+    [HttpPut("{id:int}")]
+    public async Task<ActionResult> Put(int id, Product product)
+    {
+        if (product.Id != id || !ProductExists(id))
+            return NotFound();
+        
+        _context.Entry(product).State = EntityState.Modified;
+        await _context.SaveChangesAsync();
+        
+        return NoContent();
+    }
+    
+    [HttpDelete("{id:int}")]
+    public async Task<ActionResult> Delete(int id)
+    {
+        var product = await _context.Products.FindAsync(id);
+        if (product == null) return NotFound();
+        _context.Products.Remove(product);
+        await _context.SaveChangesAsync();
+        return NoContent();
+    }
+    
+    private bool ProductExists(int id)
+    {
+        return _context.Products.Any(e => e.Id == id);
+    }
 }
