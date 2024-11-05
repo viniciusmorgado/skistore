ef819bc751caac6a4736f07960bd833fbb31a6bd
Vinicius Morgado <34577818+viniciusmorgado@users.noreply.github.com>
2024-11-05 13:24:00 -0300

    Add specification methods for GET endpoints in generic repository

--- src/SkiStore.Core/Base/Interfaces/IGenericRepository.cs
+++ src/SkiStore.Core/Base/Interfaces/IGenericRepository.cs
@@ -5,7 +5,11 @@ namespace SkiStore.Core.Base.Interfaces;
 public interface IGenericRepository<T> where T : BaseEntity
 {
     Task<T> GetByIdAsync(int id);
+    Task<T?> GetEntityWithSpec(ISpecification<T> spec);
+
     Task<IReadOnlyList<T>> GetAllAsync();
+    Task<IReadOnlyList<T>> GetAllWithSpec(ISpecification<T> spec);
+
     void Post(T entity);
     void Put(T entity);
     void Delete(T entity);
--- src/SkiStore.Infrastructure/Data/Base/Repositories/GenericRepository.cs
+++ src/SkiStore.Infrastructure/Data/Base/Repositories/GenericRepository.cs
@@ -7,38 +7,50 @@ namespace SkiStore.Infrastructure.Data.Base.Repositories;
 
 public class GenericRepository<T>(StoreContext context) : IGenericRepository<T> where T : BaseEntity
 {
+    // Specification methods
     public async Task<T> GetByIdAsync(int id)
     {
         return await context.Set<T>().FindAsync(id);
+    }
+    public async Task<T> GetEntityWithSpec(ISpecification<T> spec)
+    {
+        return await ApplySpec(spec).FirstOrDefaultAsync();
     }
 
     public async Task<IReadOnlyList<T>> GetAllAsync()
     {
         return await context.Set<T>().ToListAsync();
     }
+    public async Task<IReadOnlyList<T>> GetAllWithSpec(ISpecification<T> spec)
+    {
+        return await ApplySpec(spec).ToListAsync();
+    }
 
+    // REST Methods
     public void Post(T entity)
     {
         context.Set<T>().Add(entity);
     }
-
     public void Put(T entity)
     {
         context.Set<T>().Update(entity);
     }
-
     public void Delete(T entity)
     {
         context.Set<T>().Remove(entity);
     }
-
     public async Task<bool> SaveChangesAsync()
     {
         return await context.SaveChangesAsync() > 0;
     }
-    
     public bool Exists(int id)
     {
         return context.Set<T>().Any(x => x.Id == id);
+    }
+
+    // Helpers
+    private IQueryable<T> ApplySpec(ISpecification<T> spec)
+    {
+        return SpecsEvaluator<T>.Query(context.Set<T>().AsQueryable(), spec);
     }
 }
