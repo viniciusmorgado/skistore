e1bca206d6013201b3359750ace0cc5e267c4913
Vinicius Morgado <34577818+viniciusmorgado@users.noreply.github.com>
2025-05-05 20:09:45 -0300

    Add error component

--- client/skistore-platform/src/app/app.routes.ts
+++ client/skistore-platform/src/app/app.routes.ts
@@ -1,10 +1,12 @@
 import { Routes } from '@angular/router';
 import { ShopComponent } from './features/shop/shop.component';
 import { ProductDetailsComponent } from './features/shop/product-details/product-details.component';
+import { ErrorsComponent } from './features/errors/errors.component';
 
 export const routes: Routes = [
     {path: '', component: ShopComponent},
     {path: 'shop', component: ShopComponent},
     {path: 'shop/:id', component: ProductDetailsComponent},
+    {path: 'test-error', component: ErrorsComponent},
     {path: '**', redirectTo: '', pathMatch: 'full'}
 ];
