db3161a7190498bc7b5c3bc22a687d4203873172
Vinicius Morgado <34577818+viniciusmorgado@users.noreply.github.com>
2025-05-04 22:00:00 -0300

    Add routing and hover animation

--- client/skistore-platform/src/app/features/shop/product-details/product-details.component.ts
+++ client/skistore-platform/src/app/features/shop/product-details/product-details.component.ts
@@ -1,4 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { ShopComponent } from '../shop.component';
+import { ShopService } from '../../../core/services/shop/shop.service';
+import { ActivatedRoute } from '@angular/router';
+import { Product } from '../../../shared/models/product';
 
 @Component({
   selector: 'app-product-details',
@@ -6,6 +10,22 @@ import { Component } from '@angular/core';
   templateUrl: './product-details.component.html',
   styleUrl: './product-details.component.scss'
 })
-export class ProductDetailsComponent {
 
+export class ProductDetailsComponent implements OnInit {
+  private shopService = inject(ShopService);
+  private activatedRoute = inject(ActivatedRoute);
+  product?: Product;
+  
+  ngOnInit(): void {
+    this.loadProduct();
+  }
+
+  loadProduct() {
+    const id = this.activatedRoute.snapshot.paramMap.get('id'); // id = :id in app.routes.ts
+    if (!id) return;
+    this.shopService.getProduct(+id).subscribe({
+      next: product => this.product = product,
+      error: error => console.log(error)
+    });
+  }
 }
