5863bf2e34b334f0052be6fd4e79d09f04b9872a
Vinicius Morgado <34577818+viniciusmorgado@users.noreply.github.com>
2025-05-04 21:52:39 -0300

    Add routing and hover animation

--- client/skistore-platform/src/app/app.component.ts
+++ client/skistore-platform/src/app/app.component.ts
@@ -5,7 +5,7 @@ import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HeaderComponent, ShopComponent],
+  imports: [RouterOutlet, HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
